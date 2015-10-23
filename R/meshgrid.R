b4780d4e3d4abfd2c5752e8a620451a6dc5ff726
Viliam Simko <viliam.simko@gmail.com>
2015-10-23 19:10:53 +0200

    meshgrid documentation converted to Roxygen

--- R/meshgrid.R
+++ R/meshgrid.R
@@ -1,7 +1,22 @@
-meshgrid <-
-function (xv, yv) {
-  return(list(
-    x=outer(yv * 0, xv, FUN="+"),
-    y=outer(yv, xv * 0, FUN="+")
-    ))
+#' Rectangular grid in two dimensions
+#' 
+#' Replicates the grid vectors \code{xv} and \code{yv} to generate a full grid
+#' @author Tarik C. Gouhier (tarik.gouhier@@gmail.com)
+#' 
+#' @param xv vector of numeric values
+#' @param yv vector of numeric values
+#' 
+#' @return Returns a list containing the full grid components \code{xv} and \code{yv}:
+#' \item{x }{replicated values of xv}
+#' \item{y }{replicated values of yv}
+#' 
+#' @examples
+#' xv <- runif(10)
+#' yv <- runif(5)
+#' g <- meshgrid(xv, yv)
+meshgrid <- function (xv, yv) {
+  list(
+    x = outer(yv * 0, xv, FUN="+"),
+    y = outer(yv, xv * 0, FUN="+")
+  )
 }
