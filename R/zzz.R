038f9a577a5b6e17544789e32ca6a8387bb85ba9
Viliam Simko <viliam.simko@gmail.com>
2015-10-23 18:54:46 +0200

    removed "zzz.R" and instead created "biwavelet-package.R" file that contains description of the package, description of datasets and also the startup hook code. The Rd files in man directory will be generated using Roxygen

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    paste("biwavelet", 
-          utils::packageDescription("biwavelet",
-                                    field="Version"),
-          "loaded."),
-    appendLF = TRUE)
-}
