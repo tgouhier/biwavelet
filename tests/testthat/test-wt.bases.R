d8024f1364533d3632b37ca4085dfcac53c9a8c7
Viliam Simko <viliam.simko@gmail.com>
2015-11-17 00:11:54 +0100

    switch instead of if..elseif..else should be faster according to http://stackoverflow.com/a/7826352/855435

--- tests/testthat/test-wt.bases.R
+++ tests/testthat/test-wt.bases.R
@@ -1,4 +1,5 @@
 context("wt.bases")
+
 test_that("List of supported mother wavelets as global promise", {
 
   b <- wt.bases("morlet", c(1), 1)
@@ -6,4 +7,14 @@ test_that("List of supported mother wavelets as global promise", {
   expect_true(is.list(b))
   expect_equal(b$daughter, NA_real_)
   expect_equal(round(b$coi, 2), .73)
+})
+
+test_that("Unsupported mother wavelet should produce an error", {
+  expect_error(wt.bases("dummy", c(1), 1))
+})
+
+test_that("Supported mothers should work without errors", {
+  for (m in MOTHERS) {
+    expect_true(is.list(wt.bases(m, c(1), 1)))
+  }
 })
