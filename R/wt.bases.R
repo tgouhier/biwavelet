52a92c125238c0180f72b58423b781fe489ad3b3
Viliam Simko <viliam.simko@gmail.com>
2015-11-17 16:40:25 +0100

    Refactored "wt.bases" into three separate functions thus improving performace. The code is now ready to be reimplemented in C++ which will improve the speed even further.

--- R/wt.bases.R
+++ R/wt.bases.R
@@ -4,9 +4,9 @@
 #' 
 #' @param mother type of mother wavelet function to use. Can be set to
 #'   \code{morlet}, \code{dog}, or \code{paul}. Default is \code{morlet}.
-#' @param k vector of frequencies at which to calculate the wavelet.
-#' @param scale the wavelet scale.
-#' @param param nondimensional parameter specific to the wavelet function.
+#' @param ... see parameters \code{k}, \code{scale} and \code{param} in
+#'   functions: \code{\link{wt.bases.morlet}}, \code{\link{wt.bases.paul}} and
+#'             \code{\link{wt.bases.dog}}
 #' 
 #' @return Returns a list containing:
 #' \item{daughter}{wavelet function}
@@ -27,67 +27,73 @@
 #' # Not run: wb <- wt.bases(mother, k, scale[a1], param)
 #' 
 #' @export
-wt.bases <- function(mother = "morlet", k, scale, param = -1) {
-
-  mother <- match.arg(tolower(mother), MOTHERS)
-  n <- length(k)
-
-  # using switch instead of if..elseif..else should be faster
-  # see also http://stackoverflow.com/a/7826352/855435
+wt.bases <- function(mother = "morlet", ...) {
   switch(mother,
-    morlet = {
-      if (param == -1) {
-        param <- 6
-      }
-      k0 <- param
-
-      # TODO refactor this line to a nicer expression
-      expnt <- -(scale * k - k0) ^ 2 / 2 * (k > 0)
-
-      norm <- sqrt(scale * k[2]) * (pi ^ (-0.25)) * sqrt(n)
-      daughter <- norm * exp(expnt)
-      daughter <- daughter * (k > 0)
-      fourier.factor <- 4 * pi / (k0 + sqrt(2 + k0 ^ 2))
-      coi <- fourier.factor / sqrt(2)
-      dof <- 2
-    },
-
-    paul = {
-      if (param == -1) {
-        param <- 4
-      }
-      m <- param
-      expnt <- -(scale * k) * (k > 0)
-
-      norm <- sqrt(scale * k[2]) *
-        (2 ^ m / sqrt(m * prod(2:(2 * m - 1)))) * sqrt(n)
-
-      daughter <- norm * ((scale * k) ^ m) * exp(expnt)
-      daughter <- daughter * (k > 0)
-      fourier.factor <- 4 * pi / (2 * m + 1)
-      coi <- fourier.factor * sqrt(2)
-      dof <- 2
-    },
-
-    dog = {
-      if (param == -1) {
-        param <- 2
-      }
-      m <- param
-      expnt <- -(scale * k) ^ 2 / 2
-      norm <- sqrt(scale * k[2] / gamma(m + 0.5)) * sqrt(n)
-      daughter <- -norm * (1i ^ m) * ((scale * k) ^ m) * exp(expnt)
-      fourier.factor <- 2 * pi * sqrt(2 / (2 * m + 1))
-      coi <- fourier.factor / sqrt(2)
-      dof <- 1
-    },
-
+    morlet = wt.bases.morlet(...),
+    paul = wt.bases.paul(...),
+    dog = wt.bases.dog(...),
     stop(paste("mother wavelet parameter must be one of:",
                paste(MOTHERS, collapse = ", ")))
   )
+}
 
-  list(daughter = daughter,
+#' Helper method (not exported)
+#' @param k vector of frequencies at which to calculate the wavelet.
+#' @param scale the wavelet scale.
+#' @param param nondimensional parameter specific to the wavelet function.
+#' @return Returns a list containing:
+#' \item{daughter}{wavelet function}
+#' \item{fourier.factor}{ratio of fourier period to scale}
+#' \item{coi}{cone of influence}
+#' \item{dof}{degrees of freedom for each point in wavelet power}
+wt.bases.morlet <- function(k, scale, param = -1) {
+  k0 <- ifelse(param == -1, 6, param)
+  expnt <- -(scale * k - k0) ^ 2 / 2 * (k > 0)
+  norm <- sqrt(scale * k[2]) * (pi ^ (-0.25)) * sqrt(length(k))
+  fourier.factor <- 4 * pi / (k0 + sqrt(2 + k0 ^ 2))
+  list(daughter = norm * exp(expnt) * (k > 0),
        fourier.factor = fourier.factor,
-       coi = coi,
-       dof = dof)
+       coi = fourier.factor / sqrt(2),
+       dof = 2)
+}
+
+#' Helper method (not exported)
+#' @param k vector of frequencies at which to calculate the wavelet.
+#' @param scale the wavelet scale.
+#' @param param nondimensional parameter specific to the wavelet function.
+#' @return Returns a list containing:
+#' \item{daughter}{wavelet function}
+#' \item{fourier.factor}{ratio of fourier period to scale}
+#' \item{coi}{cone of influence}
+#' \item{dof}{degrees of freedom for each point in wavelet power}
+wt.bases.paul <- function(k, scale, param = -1) {
+  m <- ifelse(param == -1, 4, param)
+  expnt <- -(scale * k) * (k > 0)
+  norm <- sqrt(scale * k[2]) *
+          (2 ^ m / sqrt(m * prod(2:(2 * m - 1)))) * sqrt(length(k))
+  fourier.factor <- 4 * pi / (2 * m + 1)
+  list(daughter = norm * ((scale * k) ^ m) * exp(expnt) * (k > 0),
+       fourier.factor = fourier.factor,
+       coi = fourier.factor / sqrt(2),
+       dof = 2)
+}
+
+#' Helper method (not exported)
+#' @param k vector of frequencies at which to calculate the wavelet.
+#' @param scale the wavelet scale.
+#' @param param nondimensional parameter specific to the wavelet function.
+#' @return Returns a list containing:
+#' \item{daughter}{wavelet function}
+#' \item{fourier.factor}{ratio of fourier period to scale}
+#' \item{coi}{cone of influence}
+#' \item{dof}{degrees of freedom for each point in wavelet power}
+wt.bases.dog <- function(k, scale, param = -1) {
+  m <- ifelse(param == -1, 2, param)
+  expnt <- -(scale * k) ^ 2 / 2
+  norm <- sqrt(scale * k[2] / gamma(m + 0.5)) * sqrt(length(k))
+  fourier.factor <- 2 * pi * sqrt(2 / (2 * m + 1))
+  list(daughter = -norm * (1i ^ m) * ((scale * k) ^ m) * exp(expnt),
+       fourier.factor = fourier.factor,
+       coi = fourier.factor / sqrt(2),
+       dof = 1)
 }
