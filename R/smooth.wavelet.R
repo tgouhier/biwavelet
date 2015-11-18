4f1e80e38632a04ff8931edde7ca600034432885
Viliam Simko <viliam.simko@gmail.com>
2015-11-18 18:47:01 +0100

    minor change in smooth.wavelet.R

--- R/smooth.wavelet.R
+++ R/smooth.wavelet.R
@@ -40,7 +40,7 @@ smooth.wavelet <- function(wave, dt, dj, scale) {
   npad <- 2 ^ ceiling(log2(m)) # new size after padding
 
   k <- 1:as.integer(npad / 2) # faster
-  k <- k * (2 * pi / npad)
+  k <- k * 2 * pi / npad
   k <- c(0, k, -k[as.integer( (npad - 1) / 2 ):1]) # faster
 
   k2 <- k ^ 2
@@ -72,7 +72,7 @@ smooth.wavelet <- function(wave, dt, dj, scale) {
 boxcar_scale_smoothing <- function(twave, dj) {
   n <- NROW(twave)
   dj0 <- 0.6
-  dj0steps <- dj0 / (dj * 2)
+  dj0steps <- 0.5 * dj0 / dj
   dj0steps.mod <- dj0steps %% 1
   dj0steps.len <- 2 * round(dj0steps)
   ker <- c(dj0steps.mod, rep(1, length = dj0steps.len - 1), dj0steps.mod)
