a33198e84f6c537b10289d75ba5ccbbef2bd7fa6
Viliam Simko <viliam.simko@gmail.com>
2015-11-26 09:58:45 +0100

    cleaned the quantile() implementation from corFunctions-common

--- src/corFunctions-common.c
+++ src/corFunctions-common.c
@@ -1,14 +1,3 @@
-/* 
- * Common functions for fast calculations of correlations
- *
- * General notes about handling missing data, zero MAD etc:
- * The idea is that bicor should switch to cor whenever it is feasible, it helps, and it is requested:
- * (1) if median is NA, the mean would be NA as well, so there's no point in switching to Pearson
- * (2) In the results, columns and rows corresponding to input with NA means/medians are NA'd out.
- * (3) The convention is that if zeroMAD is set to non-zero, it is the index of the column in which MAD is
- *     zero (plus one for C indexing)
- */ 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -23,24 +12,33 @@
  * the appropriate quantile of the remaining (finite) entries.
  * 
  * q is the quantile: 1/2 will give exactly the median above.
+ * 
+ * General notes about handling missing data, zero MAD etc:
+ * The idea is that bicor should switch to cor whenever it is feasible, it helps, and it is requested:
+ * (1) if median is NA, the mean would be NA as well, so there's no point in switching to Pearson
+ * (2) In the results, columns and rows corresponding to input with NA means/medians are NA'd out.
+ * (3) The convention is that if zeroMAD is set to non-zero, it is the index of the column in which MAD is
+ *     zero (plus one for C indexing)
  */
-double quantile(double * x, const size_t n, const double q, int * err) {
+double quantile(double *x, const size_t n, const double q, int *err) {
+  
   *err = 0;
   
   // Put all NA's at the end.
   size_t bound = n;
-  for (size_t i = n; i>0; ) 
-  {
+  for (size_t i = n; i>0;) {
     i--;
-    if (ISNAN(x[i]))
-    {
+    if (ISNAN(x[i])) {
        bound--;
        x[i] = x[bound];
        x[bound] = NA_REAL;
     }
   }
 
-  return bound == 0 // Any non-NA's left?
-    ? NA_REAL
-    : pivot(x, bound, ( 1.0 * (bound - 1)) * q);
+  // Any non-NA's left?
+  if(bound == 0) {
+    return NA_REAL;
+  }
+
+  return pivot(x, bound, ( 1.0 * (bound - 1)) * q);
 }
--- src/corFunctions-common.h
+++ src/corFunctions-common.h
@@ -1,10 +1,7 @@
-/* 
- * Common functions for fast calculations of correlations
- */
 #ifndef __corFunctions_common_h__
 #define __corFunctions_common_h__
 #include "pivot.h"
 
-enum { noWarning, warnZeroMAD };
 double quantile(double * x, const size_t n, const double q, int * err);
+
 #endif
