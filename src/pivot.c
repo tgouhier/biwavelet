19458595ba440a4412ebf875a6f5eebe4ffbd810
Viliam Simko <viliam.simko@gmail.com>
2015-11-26 09:59:02 +0100

    cleaned pivor() implementation

--- src/pivot.c
+++ src/pivot.c
@@ -10,31 +10,36 @@
 
 #include "pivot.h"
 
-double vMax(double * v, size_t len)
-{
+double vMax(double *v, const size_t len) {
   double mx = v[0];
-  for (size_t i=1; i<len; i++)
-    if (v[i] > mx) mx = v[i];
+  for (size_t i=1; i<len; i++) {
+    if (v[i] > mx) {
+      mx = v[i];
+    }
+  }
   return mx;
 }
 
-double vMin(double * v, size_t len)
-{
+double vMin(double * v, const size_t len) {
   double mn = v[0];
-  for (size_t i=1; i<len; i++)
-    if (v[i] < mn) mn = v[i];
+  for (size_t i = 1; i < len; i++) {
+    if (v[i] < mn) {
+      mn = v[i];
+    }
+  }
   return mn;
 }
 
-
-double pivot(double * v, size_t len, double target)
-{
-  if (len > 2)
-  {
+double pivot(double * v, const size_t len, const double target) {
+  if (len > 2) {
+    
     // pick the pivot, say as the median of the first, middle and last
-    size_t i1 = 0, i2 = len-1, i3 = (len-1)/2, ip;
-    if (v[i1] <= v[i2])
-    {
+    const size_t i1 = 0;
+    const size_t i2 = len - 1;
+    const size_t i3 = (len - 1) / 2;
+    size_t ip;
+    
+    if (v[i1] <= v[i2]) {
       if (v[i2] <= v[i3])
         ip = i2;
       else if (v[i3] >= v[i1])
@@ -52,57 +57,52 @@ double pivot(double * v, size_t len, double target)
 
     // put ip at the end
     double vp = v[ip];
-    v[ip] = v[len-1];
-    v[len-1] = vp;
-
-    // Rprintf("   pivot value: %5.3f, index: %d\n", vp, ip);
+    v[ip] = v[len - 1];
+    v[len - 1] = vp;
 
     // pivot everything else
     size_t bound = 0;
-    for (size_t i=0; i<len; i++) if (v[i] < vp)
-    {
-      double x = v[bound];
-      v[bound] = v[i];
-      v[i] = x;
-      bound++;
+    for (size_t i=0; i<len; i++) {
+      if (v[i] < vp) {
+        const double x = v[bound];
+        v[bound] = v[i];
+        v[i] = x;
+        bound++;
+      }
     }
 
     v[len-1] = v[bound];
     v[bound] = vp;
 
     // Did we find the target?
-    double crit = target - bound;
+    const double crit = target - bound;
     
-    // Rprintf("   crit: %5.3f\n", crit);
-    if (fabs(crit) > 1.0)
-    {
+    if (fabs(crit) > 1.0) {
       if (crit < 0)
         return pivot(v, bound, target);
       else
         return pivot(v+bound+1, len-bound-1, target-bound-1);
     }
-    // Rprintf("vMax(v, bound): %5.3f, vMin(v+bound+1, len-bound-1): %5.3f, vp: %5.3f\n", vMax(v, bound),
-                // vMin(v+bound+1, len-bound-1), vp);
-    if (crit < 0)
-    {
-       double v1 = vMax(v, bound);
-       return (v1 *(-crit) + vp * (1+crit));
+
+    if (crit < 0) {
+       const double v1 = vMax(v, bound);
+       return (v1 * (-crit) + vp * (1 + crit));
     } // else
-    double v2 = vMin(v+bound+1, len-bound-1);
-    return (vp * (1-crit) + v2 * crit);
-  } 
-  else if (len==2)
-  {
-      // Rprintf("  Short v, returning a direct value.\n"); 
-      double v1 = vMin(v, 2);
-      double v2 = vMax(v, 2);
-      if (target < 0) return v1;
-      else if (target > 1) return v2;
-      else return (target * v2 + (1-target) * v1);
-  }
-  else 
-  {
-     // Rprintf("  length 1 v, returning a direct value.\n"); 
-     return v[0];
-  }
+    const double v2 = vMin(v + bound + 1, len - bound - 1);
+    return (vp * (1 - crit) + v2 * crit);
+    
+  } else if (len == 2) {
+
+      const double v1 = vMin(v, 2);
+      const double v2 = vMax(v, 2);
+      
+      if (target < 0) {
+        return v1;
+      } else if (target > 1) {
+        return v2;
+      } else {
+        return target * v2 + (1-target) * v1;
+      }
+  } // else
+  return v[0];
 }
--- src/pivot.h
+++ src/pivot.h
@@ -1,9 +1,9 @@
 #ifndef __pivot_h__
 #define __pivot_h__
 
-double vMax(double * v, size_t len);
-double vMin(double * v, size_t len);
-double pivot(double * v, size_t len, double target);
+double vMax(double * v, const size_t len);
+double vMin(double * v, const size_t len);
+double pivot(double * v, const size_t len, const double target);
 
 #endif
 
