e79db3210aec98cd213c2a724b0a56e4097e5131
Viliam Simko <viliam.simko@gmail.com>
2015-11-27 16:56:27 +0100

    first attempt to improve arima.sim function from R

--- inst/benchmarks/profviz-wtc_sig.R
+++ inst/benchmarks/profviz-wtc_sig.R
@@ -47,3 +47,70 @@ microbenchmark(
   },
   times = 1000
 )
+
+ntimesteps <- 100L
+lag1 <- c(-0.0693, 0.1581)
+
+microbenchmark(
+  function() {
+    d1  <- cbind(1:ntimesteps,
+                 arima.sim(model = list(ar = lag1[1], ma = 0), n = ntimesteps))
+    d2  <- cbind(1:ntimesteps,
+                 arima.sim(model = list(ar = lag1[2], ma = 0), n = ntimesteps))
+  },
+  function() {
+    d1  <- cbind(1:ntimesteps,
+                 arima.sim(model = list(ar = lag1[1], ma = 0), n = ntimesteps))
+    d2  <- d1
+    d2[,2] <- cbind(1:ntimesteps,
+                    arima.sim(model = list(ar = lag1[2], ma = 0), n = ntimesteps))
+  }
+      
+  times = 100000
+)
+
+get_minroots <- function(ar) {
+  min(Mod(polyroot(c(1, -ar))))
+}
+
+ar1_ma0_sim <- function(minroots, n) {
+  
+  if (minroots <= 1) {
+    stop("'ar' part of model is not stationary")
+  }
+
+  rand.gen <- rnorm
+  innov <- rand.gen(n)
+  n.start <- NA
+  start.innov <- rand.gen(n.start)
+  
+  p <- 1 #length(model$ar)
+  q <- 1 #length(model$ma)
+
+  if (is.na(n.start)) {
+    n.start <- p + q + ceiling(6 / log(minroots))
+  }
+  
+  if (n.start < p + q)
+    stop("burn-in 'n.start' must be as long as 'ar + ma'")
+  
+  if (!missing(start.innov) && length(start.innov) < n.start) {
+    stop(sprintf(ngettext(n.start, "'start.innov' is too short: need %d point", 
+                          "'start.innov' is too short: need %d points"), n.start), 
+         domain = NA)
+  }
+
+  x <- ts(data = c(start.innov[seq_len(n.start)], innov[1L:n]),
+          start = 1 - n.start)
+  
+  x <- filter(x, model$ar, method = "recursive")
+  
+  if (n.start > 0) {
+    x <- x[-(seq_len(n.start))]
+  }
+  
+  as.ts(x)
+}
+
+
+
