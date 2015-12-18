24445bb0ef048d59b5d1617197fa8b4bf846e842
Viliam Simko <viliam.simko@gmail.com>
2015-12-18 17:51:59 +0100

    added test to `convolve2D`

--- tests/testthat/test-convolve2D.R
+++ tests/testthat/test-convolve2D.R
@@ -10,4 +10,12 @@ test_that("Original convolve2D should be equal to convolve2D_typeopen", {
     convolve2D(x,y, type = "open"))
 })
 
+test_that("Circular convolve2D should identify length mismatch", {
+  x <- matrix(1, nrow = 10, ncol = 3)
+  y <- c(1,1,2)
+  expect_error(
+    convolve2D(x,y, type = "circular"),
+    regexp = "length mismatch")
+})
+
 
