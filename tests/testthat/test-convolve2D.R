dd200b90558e79c4dadf28540ea4e3b0cd237d53
Viliam Simko <viliam.simko@gmail.com>
2015-12-18 17:07:19 +0100

    fixed broken test for `convolve2D`

--- tests/testthat/test-convolve2D.R
+++ tests/testthat/test-convolve2D.R
@@ -3,6 +3,8 @@ context("Fast column-wise convolution of a matrix (convolve2D)")
 # Tests ==========
 
 test_that("Original convolve2D should be equal to convolve2D_typeopen", {
+  x <- matrix(1, nrow = 10, ncol = 3)
+  y <- c(1,1,2)
   expect_equal(
     convolve2D_typeopen(x,y),
     convolve2D(x,y, type = "open"))
