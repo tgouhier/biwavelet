78ccfdd8f72268b1972a85dc3835bc7fb1df6c26
Viliam Simko <viliam.simko@gmail.com>
2015-12-18 18:24:00 +0100

    added test for `pwtc`

--- tests/testthat/test-plot.biwavelet.R
+++ tests/testthat/test-plot.biwavelet.R
@@ -13,11 +13,21 @@ test_that("Plotting in black and white should work", {
   expect_null( plot.biwavelet(some_wt, bw = TRUE) )
 })
 
-test_that("Plotting wavelet cogerence (wtc) should work", {
+test_that("Wavelet cogerence (wtc) should work", {
   t1 <- cbind(1:100, rnorm(100))
   t2 <- cbind(1:100, rnorm(100))
   wtc.t1t2 <- wtc(t1, t2, nrands = 10, quiet = TRUE)
   expect_null( plot.biwavelet(wtc.t1t2) )
+})
+
+test_that("Partial wavelet cogerence (pwtc) should work", {
+  y <- cbind(1:100, rnorm(100))
+  x1 <- cbind(1:100, rnorm(100))
+  x2 <- cbind(1:100, rnorm(100))
+  
+  pwtc.yx1 <- pwtc(y, x1, x2, nrands = 0)
+  expect_equal(class(pwtc.yx1), "biwavelet")
+  expect_null( plot.biwavelet(pwtc.yx1) )
 })
 
 test_that("Setting ylim value", {
