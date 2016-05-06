ac06a3e985b7367b6543a021f6cacee272d1a881
Viliam Simko <viliam.simko@gmail.com>
2016-05-06 17:58:36 +0200

    refactoring R function row_quantile

--- R/row_quantile.R
+++ R/row_quantile.R
@@ -1,24 +1,21 @@
 #' This function calls the C++ implementation of row quantile.
-#' 
+#'
 #' @param data Numeric matrix whose row quantiles are wanted.
 #' @param p Probability with value in [0,1]
 #' @return A vector of length \code{nrows(data)}, where each element represents
 #'   row quantile.
 row_quantile <- function(data, p) {
 
-  data <- as.matrix(data)
-  ncol <- ncol(data);
-  nrow <- nrow(data);
-  quantiles <- rep(0, nrow);
-
-  p <- as.numeric(as.character(p));
+  # fail fast
   if (length(p) > 1) {
     stop("This function only calculates one quantile at a time, for now.");
   }
 
-  res <- .C("rowQuantileC", data = as.double(data),
-            nrow = as.integer(nrow), ncol = as.integer(ncol),
-            p = as.double(p), quantiles = as.double(quantiles), NAOK = TRUE)
+  data <- as.matrix(data)
+  nrow <- nrow(data);
 
-  res$quantiles;
+  .C("rowQuantileC", data = as.double(data),
+    nrow = nrow, ncol = ncol(data), p = as.double(p),
+    quantiles = rep(0, nrow), NAOK = TRUE
+  )$quantiles
 }
